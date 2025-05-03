4dc650137b8f3ce717e7f9dd3a985338bff47882
Kamilla Amiri <152669031+kamillaurunboeva@users.noreply.github.com>
2025-05-02 23:14:44 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  Small animation
+//
+//  Created by Kamilla Urunbaeva on 12/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("lightsOn") private var lightsOn: Bool =
+    false
+    private var backgroundColor: Color {
+        lightsOn ? .white : .black
+    }
+    private var imageName: String {
+        lightsOn ? "sun.max.fill" : "moon.fill"
+    }
+    private var imageColor: Color {
+        lightsOn ? .yellow : .blue
+    }
+    private var text: String {
+        lightsOn ? "Turn the light off!" : "Turn the light on!"
+    }
+    @State private var rotation: Double = 0
+    
+    var body: some View {
+        ZStack {
+            backgroundColor.ignoresSafeArea(.all)
+            
+            VStack(alignment: .center) {
+                Spacer()
+                Image(systemName: imageName)
+                    .font(.system(size: 88))
+                    .foregroundColor(imageColor)
+                    .padding()
+                    .rotationEffect(.degrees(rotation))
+                    .animation(.easeInOut(duration: 1))
+                Spacer()
+                Text(text)
+                    .font(.largeTitle)
+                    .bold()
+                    .animation(.none)
+                Toggle("", isOn: $lightsOn)
+                    .labelsHidden()
+            }
+            .foregroundColor(lightsOn ? .black : .white)
+            .onChange(of: lightsOn, perform: {
+                value in
+                if value {
+                    rotation += 360
+                } else {
+                    rotation -= 360
+                }
+            })
+        }
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Small_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Small_animationApp.swift
+//  Small animation
+//
+//  Created by Kamilla Urunbaeva on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Small_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
